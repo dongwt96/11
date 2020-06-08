bca9e8a23b522664188e9e62a7164f41f993cf6f
dong <643133884@qq.com>
2020-06-08 08:42:42 +0800

    boot test

---
+++ src/test/java/com/dongwt/boot81/Boot81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongwt.boot81;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot81ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
